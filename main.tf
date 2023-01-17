75d5ede1b259b76f5acab6b2393cc61ecf349676
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-17 22:18:38 +0400

    first commit

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
+variable "instance_name" {
+  type        = string
+  description = "The name tag of the ec2 instance"
+}
 
 # resource "aws_s3_bucket" "tf_state_bucket" {
 #   bucket = "bucketforterraformangitGIThub"
