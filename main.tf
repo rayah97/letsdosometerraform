4530a67b0462edccf44969c19fad4cd72cf853c0
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-21 01:30:53 +0400

    first commit

--- main.tf
+++ main.tf
@@ -63,7 +63,6 @@ resource "aws_security_group" "main" {
 resource "aws_instance" "main" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name = "my_key"
 
   network_interface {
     network_interface_id = aws_network_interface.main.id
