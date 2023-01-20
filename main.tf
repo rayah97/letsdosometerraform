8c55703f9834848d8516555689787a11b47fbc23
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-21 02:07:44 +0400

    first commit

--- main.tf
+++ main.tf
@@ -74,29 +74,29 @@ resource "aws_instance" "main" {
     Name = "${var.name_prefix}-instance"
   }
 }
-resource "aws_internet_gateway" "example" {
-  vpc_id = aws_vpc.example.id
-}
+# resource "aws_internet_gateway" "example" {
+#   vpc_id = aws_vpc.example.id
+# }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
-  map_public_ip_on_launch = true
-}
+# resource "aws_subnet" "public_subnet" {
+#   vpc_id                  = aws_vpc.example.id
+#   cidr_block              = "10.0.1.0/24"
+#   availability_zone       = "us-west-2a"
+#   map_public_ip_on_launch = true
+# }
 
-resource "aws_route_table" "public_subnet" {
-  vpc_id = aws_vpc.example.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
-  }
-}
+# resource "aws_route_table" "public_subnet" {
+#   vpc_id = aws_vpc.example.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.example.id
+#   }
+# }
 
-resource "aws_route_table_association" "public_subnet" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_subnet.id
-}
+# resource "aws_route_table_association" "public_subnet" {
+#   subnet_id      = aws_subnet.public_subnet.id
+#   route_table_id = aws_route_table.public_subnet.id
+# }
 
 
  terraform {
