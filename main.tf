76ec00c25e29c89f89053893bea63c4ebdc7b938
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-22 02:02:53 +0400

    first commit

--- main.tf
+++ main.tf
@@ -38,6 +38,12 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
