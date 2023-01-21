82558fcd00f51f4f42604e732942521d7fd0fbe1
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-21 18:26:20 +0400

    first commit

--- main.tf
+++ main.tf
@@ -51,10 +51,10 @@ resource "aws_instance" "main" {
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-  network_interface {
-    network_interface_id = aws_network_interface.main.id
-    device_index         = 0
-  }
+  # network_interface {
+  #   network_interface_id = aws_network_interface.main.id
+  #   device_index         = 0
+  # }
   tags = {
     Name = "${var.name_prefix}-instance"
   }
