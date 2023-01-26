98cba60953c9351f1b197d9b0408c91b0cab352c
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-26 17:51:54 +0400

    fixed millionare

--- init-script.sh
+++ init-script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-                sudo apt-get update
-		sudo apt-get install -y apache2
-		sudo systemctl start apache2
-		sudo systemctl enable apache2
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+        sudo yum update -y
+		sudo yum install -y httpd.x86_64
+		sudo systemctl start httpd.service
+		sudo systemctl enable httpd.service		
+		# echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
--- main.tf
+++ main.tf
@@ -52,9 +52,23 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
 
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
 
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
 
 resource "aws_instance" "main" {
   ami           = var.ami
